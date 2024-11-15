45236ca3cb80b2a12dcfd3550d3ab895b946c1a7
AsoBot <asobot2024@gmail.com>
2024-11-16 06:06:08 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -28,13 +28,23 @@ namespace AsoBotCar {
         }
     }
 
-    //% block="|%number|％で後退"
+    //% block="移動|%number|％で前進"
+    //% back_percent.defl=50
+    export function forward(forward_percent: number = 50) {
+        // 入力値を0〜100に制限
+        forward_percent = Math.min(100, Math.max(0, forward_percent));
+
+        pins.servoWritePin(AnalogPin.P8, 90 - 90 * (forward_percent / 100));
+        pins.servoWritePin(AnalogPin.P15, 90 + 90 * (forward_percent / 100));
+    }
+
+    //% block="移動|%number|％で後退"
     //% back_percent.defl=50
     export function back(back_percent: number = 50) {
         // 入力値を0〜100に制限
         back_percent = Math.min(100, Math.max(0, back_percent));
 
-        servos.P0.run(90 + 90 * (back_percent / 100));
+        pins.servoWritePin(AnalogPin.P8, 90 + 90 * (back_percent / 100));
         pins.servoWritePin(AnalogPin.P15, 90 - 90 * (back_percent / 100));
     }
 
