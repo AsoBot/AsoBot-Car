7f88b5f0a9ddc029eb7e273597819166013c69ac
AsoBot <asobot2024@gmail.com>
2024-11-21 19:56:15 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,5 @@
 //% color=#1299AF weight=100 icon="\f544" block="AsoBot-Car"
-namespace AsoBotCar {
-    function lcdWriteRegister(reg: number, val: number) {
-        let lcdAddr = 0x3e
-        let cmd = (reg << 8) | val
-        pins.i2cWriteNumber(lcdAddr, cmd, NumberFormat.UInt16BE)
-    }
-
-    //% block="イニシャライズ"
-    export function lcdInit() {
-        basic.pause(30)
-        lcdWriteRegister(0x80, 0x20)
-        basic.pause(1);
-        lcdWriteRegister(0x80, 0x0c)
-        basic.pause(1)
-        lcdClearScreen()
-    }
-
-    //% block="画面をクリア"
-    export function lcdClearScreen() {
-        lcdWriteRegister(0x80, 0x01)
-        basic.pause(2)
-    }
-
-    //% block="文字列|%text|を表示"
-    export function lcdWriteText(text: string) {
-        for (let index = 0; index <= text.length; index++) {
-            lcdWriteRegister(0x40, text.charCodeAt(index))
-        }
-    }
+namespace asobotcar {
 
     //% block="停止"
     export function stop() {
